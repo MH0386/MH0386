54d10d657abd0be0369504bf1abe58ddde2e531a
Mohamed Hisham Abdelzaher <11410120200483@stud.cu.edu.eg>
2023-11-07 10:20:11 +0200

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,5 +1,5 @@
 from pdflatex import PDFLaTeX
 
 pdfl = PDFLaTeX.from_texfile("resume.tex")
-pdf, log, completed_process = pdfl.create_pdf(keep_log_file=True)
+pdf, log, completed_process = pdfl.create_pdf()
 print(pdf)
